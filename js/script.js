a9d37582fa5029bb675791d092ef810acd78fcd0
themimicruster-hub <themimicruster@gmail.com>
2026-07-16 04:39:58 -0700

    Upload pertama

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function mulaijumpscare() {
+
+    const jumpscare =
+document.getElementById("jumpscare");
+    const suara =
+document.getElementById("suara");
+
+    jumpscare.style.display = "block";
+
+    suara.play();
+}
